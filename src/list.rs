226633753748f79832b37a3aba9946ce1948de92
Jason <m.jason.liu@outlook.com>
2019-06-21 10:05:11 +0800

    6.21,2019#1

---
+++ src/list.rs
@@ -0,0 +1,95 @@
+use std::mem;
+
+pub struct List<T> {
+    pub head: Option<Node<T>>,
+    len: usize
+}
+
+impl <T> List<T> {
+    pub fn new () -> Self {
+        List {
+            head: None,
+            len: 0,
+        }
+    }
+
+    pub fn push_end(&mut self, data: T) {
+        if self.len == 0 {
+            self.head = Some(Node {
+                data,
+                next: None,
+            })
+        } else {
+            let mut temp: &mut Node<T> = self.head.as_mut().unwrap();
+
+            while let Some(ref mut n) = temp.next {
+                temp = &mut *n;
+            }
+
+            temp.next = Some(Box::new(Node{
+                data,
+                next: None,
+            }))
+        }
+        self.len += 1;
+    }
+
+    pub fn pop_end(&mut self) -> Option<T> {
+        if self.len == 0 {
+            return None
+        }
+        else {
+            let mut temp: *mut Node<T> = self.head.as_mut().unwrap();
+            let mut second_last: *mut Node<T> = temp as *mut Node<T>;
+
+            unsafe {
+                while let Some(ref mut n) = (*temp).next {
+                    second_last = temp;
+                    temp = n.as_mut() as *mut Node<T>;
+                }
+
+                (*second_last).next = None;
+                Some(mem::replace(&mut (*temp).data, mem::zeroed()))
+            }
+        }
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            current: match self.head {
+                Some(ref n) => Some(Box::new(n)),
+                None => None
+            }
+        }
+    }
+}
+
+pub struct Node<T> {
+    pub data: T,
+    pub next: Option<Box<Node<T>>>,
+}
+
+pub struct Iter<'a, T> {
+    pub current: Option<Box<&'a Node<T>>>
+}
+
+impl <'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let r = match &self.current {
+            Some(n) => Some(&n.data),
+            None => None
+        };
+        self.current = match self.current.as_ref() {
+            Some(c) => {
+                match c.next{
+                    Some(ref n) => Some(Box::new(&*n)),
+                    None => None
+                }
+            },
+            None => None
+        };
+        r
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,85 @@
+mod list;
+
+fn main() {
+    let mut l = list::List::new();
+    l.push_end("hello");
+    l.push_end("world");
+    l.push_end("pretty doh");
+    l.pop_end();
+//    l.pop_end();
+//    l.pop_end();
+
+
+    for i in l.iter().enumerate() {
+        println!("{}: {}", i.0, i.1)
+    }
+}
+
+//fn main() {
+//    struct List {
+//        head: Option<Node>,
+//        len: usize
+//    }
+//    impl List{
+//        fn new()->Self {
+//            List{
+//                head: None,
+//                len: 0
+//            }
+//        }
+//
+//        fn push_end(&mut self, data:String) {
+//            if self.len == 0 {
+//                self.head = Some(Node {
+//                    data,
+//                    next: None,
+//                })
+//            } else {
+//                let mut temp = self.head.as_mut().unwrap();
+//
+////                while let Some(n) = &mut temp.next {
+////                    temp = n.next.as_mut().unwrap()
+////                }
+//                while let Some(ref mut n) = temp.next {
+//                    temp = n.next.as_mut().unwrap()
+//                }
+//
+//                temp.next = Some(Box::new(Node{
+//                    data,
+//                    next: None,
+//                }))
+//            }
+//            self.len += 1;
+//        }
+//    }
+//
+//    struct Node {
+//        data: String,
+//        next: Option<Box<Node>>
+//    }
+//
+//    let list = List::new();
+//}
+
+
+//
+//use std::mem;
+//
+//fn main () {
+//    #[derive(Debug)]
+//    struct Demo {
+//        s: String
+//    }
+//
+//    let mut d: &mut Demo = &mut Demo{
+//        s: String::from("hello world"),
+//    };
+//
+//    let b = unsafe{
+//        mem::replace(d, mem::zeroed())
+//    };
+//
+//    println!("{:?}", b);
+//
+//    ;
+//}
